ff7b1f7e6bd0d8741ef5e00f0e5796ae5b54f91e
muhammadhilmiathallah <hilmiathallah.1@gmail.com>
2023-11-30 22:25:53 +0700

    update

--- src/main/java/com/uts/jwp/Domain/Lecture.java
+++ src/main/java/com/uts/jwp/Domain/Lecture.java
@@ -1,9 +1,20 @@
 package com.uts.jwp.Domain;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class Lecture {
+    @NotBlank(message = "nip is required")
+    // @Pattern(regexp = "LCT", message = "must be LCT")
     private String nip;
+    @NotBlank(message = "full name is required")
+    @Size(min = 3, max = 50)
     private String fullName;
+    @Email(message = "email must @ and is required")
+    @NotBlank(message = "email is required")
     private String email;
+    @NotBlank(message = "phone number is required")
     private String phoneNumber;
 
     public String getNip() {
